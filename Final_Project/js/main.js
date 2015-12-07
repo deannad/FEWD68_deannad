3f059a0bf889f47b71cada74da7ab5480cc79d4a
deannad <deanna.destefano@gmail.com>
2015-12-07 18:08:15 -0500

    Final Project v2
    Final project with some refactoring.

--- Final_Project/js/main.js
+++ Final_Project/js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
   });
 
 
-  var controller = new ScrollMagic.Controller({globalSceneOptions: {reverse: true, triggerHook:.15}});
+  var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook:.15}});
   controller.scrollTo(function(target) {
     TweenMax.to(window, 0.5, {
     scrollTo : {
@@ -22,56 +22,54 @@ $(document).ready(function() {
   });
 
   //  Bind scroll to anchor links
-$(document).on("click", "a[href^=#]", function(e) {
-  var id = $(this).attr("href"); // grab the href attribute value
+  $(document).on("click", "a[href^=#]", function(e) {
+    var id = $(this).attr("href") + " .anchor"; // grab the href attribute value
 
-  if($(id).length > 0) {
-    e.preventDefault(); // prevents default behavior of links.
-    // trigger scroll
-    controller.scrollTo(id);
-        // If supported by the browser we can also update the URL
-  }
-});
+    if($(id).length > 0) {
+      e.preventDefault(); // prevents default behavior of links.
+      // trigger scroll
+      controller.scrollTo(id);
+          // If supported by the browser we can also update the URL
+    }
+  });
   
+  var scenes = [
+  ];
 
-  var scene1 = new ScrollMagic.Scene({triggerElement: "#section1", duration: $("#section1").height()}) // point of execution
-    .setClassToggle("#menuItem1", "active") // add class toggle
-    .addIndicators(); // add indicators (requires plugin)
+  for (var i = 1; i <= 6; i++) {
+    var scene = new ScrollMagic.Scene({
+      triggerElement: "#section"+ i,
+      duration: $("#section"+i).height()
+      }) // point of execution
+    .setClassToggle("#menuItem"+i, "active") // add class toggle
+    scenes.push(scene);
+  }
 
-  var scene2 = new ScrollMagic.Scene({triggerElement: "#section2", duration: $("#section2").height()}) // point of execution
-    .setClassToggle("#menuItem2", "active") // add class toggle
-    .addIndicators(); // add indicators (requires plugin)
-
-  var scene3 = new ScrollMagic.Scene({triggerElement: "#section3", duration: $("#section3").height()}) // point of execution
-    .setClassToggle("#menuItem3", "active") // add class toggle
-    .addIndicators(); // add indicators (requires plugin)
-
-  var scene4 = new ScrollMagic.Scene({triggerElement: "#section4", duration: $("#section4").height()}) // point of execution
-    .setClassToggle("#menuItem4", "active") // add class toggle
-    .addIndicators(); // add indicators (requires plugin)
-
-  var scene5 = new ScrollMagic.Scene({triggerElement: "#section5", duration: $("#section5").height()}) // point of execution
-    .setClassToggle("#menuItem5", "active") // add class toggle
-    .addIndicators(); // add indicators (requires plugin)
-
-  var scene6 = new ScrollMagic.Scene({triggerElement: "#section6", duration: $("#section6").height()}) // point of execution
-    .setClassToggle("#menuItem6", "active") // add class toggle
-    .addIndicators(); // add indicators (requires plugin)
+  controller.addScene(scenes);
 
 
-  controller.addScene([
-  scene1,
-  scene2,
-  scene3,
-  scene4,
-  scene5,
-  scene6
-  ]);
+  var data = $( "#formSection" ).on( "submit", function( event ) {
+    event.preventDefault();
+    console.log( $( "input" ).val() );
 
-  $("#userQuestion").keyup(function(){
-    alert(this.value);
+    if ($( "input" ).val().indexOf("?") == -1) {
+      $( "#formSection div" ).text( "That's not a question. Please, send a question." );
+      $( "#formSection div" ).removeClass("invisible");
+      $("#userQuestion").val("");
+    } else {
+      $( "#formSection div" ).text( "Thanks for submitting your question!" );
+      $( "#formSection div" ).removeClass("invisible");
+      $("#userQuestion").val("");
+    }
   });
 
+  // if (data == "a") {
+  //   alert("You did it!");
+
+  // } else {
+  //   alert("Nope :/");
+
+  // }
 
 });
 
